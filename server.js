237db0be9bcc3281e3b73ac269c4a486727a8f7b
thoranderskow <tanderskow@gmail.com>
2018-09-14 17:33:52 -0700

    finished weather app tutorial

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
-//EXPRESS JS BOILERPLATE & EJS & BODYPARSER
-const express = require('express')
-const app = express()
+const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
+const app = express()
+
+const apiKey = 'a9339c13d16cdfc1da51c5082c45cf79';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index')
+  res.render('index', {weather: null, error: null});
 })
+
 app.post('/', function (req, res) {
-  res.render('index');
-  console.log(req.body.city);
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
 })
 
 app.listen(3000, function () {
